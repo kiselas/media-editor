18e92cc9e925596df95ce0ea8a0ae4ab211ff9a5
Aleksandr Kiselev <a.kiselev@brandquad.ru>
2023-01-15 23:36:23 +0300

    Add secure settings

--- django_media_editor/settings.py
+++ django_media_editor/settings.py
@@ -196,8 +196,11 @@ CSRF_COOKIE_SAMESITE = 'Strict'
 SESSION_COOKIE_SECURE = True
 SECURE_BROWSER_XSS_FILTER = True
 SECURE_CONTENT_TYPE_NOSNIFF = True
-# SECURE_SSL_REDIRECT = True
+SECURE_SSL_REDIRECT = True
 X_FRAME_OPTIONS = 'DENY'
-# SECURE_HSTS_SECONDS = 300  # set low, but when site is ready for deployment, set to at least 15768000 (6 months)
-# SECURE_HSTS_INCLUDE_SUBDOMAINS = True
-# SECURE_HSTS_PRELOAD = True
+SECURE_HSTS_SECONDS = 15768000  # set low, but when site is ready for deployment, set to at least 15768000 (6 months)
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
+
+CSRF_TRUSTED_ORIGINS = ['https://media-editor.ru']
+
