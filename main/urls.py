d460a24bcfe155f20b7261a97042f7be8dc8d027
Aleksandr Kiselev <a.kiselev@brandquad.ru>
2023-01-17 20:56:27 +0300

    Add robots.txt

--- main/urls.py
+++ main/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.views.generic import TemplateView
 
 from . import views
 
@@ -6,4 +7,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('download/<str:file_id>', views.get_compressed_file, name='compress_file_unique_url'),
     path('sitemap.xml', views.get_sitemap, name='sitemap'),
+    path("robots.txt", TemplateView.as_view(template_name="robots.txt", content_type="text/plain"), ),
 ]
