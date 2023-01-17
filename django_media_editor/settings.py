558e3c67958e83f412a32156631bc9ba9cfbdf52
Aleksandr Kiselev <a.kiselev@brandquad.ru>
2023-01-17 20:59:45 +0300

    Disable debug

--- django_media_editor/settings.py
+++ django_media_editor/settings.py
@@ -29,7 +29,7 @@ environ.Env.read_env(BASE_DIR / '.env')
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env('DEBUG')
+DEBUG = env.bool('DEBUG')
 
 ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS')
 
