c4514bc34b820c79ab8b1e644a49cd1261874eac
Aleksandr Kiselev <a.kiselev@brandquad.ru>
2023-01-17 20:32:00 +0300

    Add sitemap

--- main/views.py
+++ main/views.py
@@ -1,6 +1,7 @@
 from django.core.cache import cache
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.template import loader
 
 from compressor.utils import get_path_to_file
 from django_media_editor.constants import FileStatus, FULL_PATH_TO_PROCESSED_FILES
@@ -24,5 +25,9 @@ def get_compressed_file(request, file_id):
                                                         "file_status": file_status,
                                                         "path_to_file": path_to_file})
 
+
 def get_sitemap(request, **kwargs):
-    return render(request, 'sitemap.xml')
+    t = loader.get_template('sitemap.xml')
+    response = HttpResponse(t.render(), content_type="application/xml")
+    response['Content-Disposition'] = 'attachment; filename=sitemap.xml'
+    return response
