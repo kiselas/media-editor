24ee9de4bc1bd16b8737cc266f8c1bcf489a1dcb
Aleksandr Kiselev <a.kiselev@brandquad.ru>
2023-01-22 13:46:50 +0300

    Add sentry to project

--- django_media_editor/settings.py
+++ django_media_editor/settings.py
@@ -206,24 +206,24 @@ SECURE_HSTS_PRELOAD = True
 
 CSRF_TRUSTED_ORIGINS = ['https://media-editor.ru']
 
+if not DEBUG:
+    sentry_sdk.init(
+        dsn=os.getenv('SENTRY_DSN'),
+        integrations=[DjangoIntegration()],
 
-sentry_sdk.init(
-    dsn=os.getenv('SENTRY_DSN'),
-    integrations=[DjangoIntegration()],
+        # Set traces_sample_rate to 1.0 to capture 100%
+        # of transactions for performance monitoring.
+        # We recommend adjusting this value in production,
+        traces_sample_rate=1.0,
 
-    # Set traces_sample_rate to 1.0 to capture 100%
-    # of transactions for performance monitoring.
-    # We recommend adjusting this value in production,
-    traces_sample_rate=1.0,
+        # If you wish to associate users to errors (assuming you are using
+        # django.contrib.auth) you may enable sending PII data.
+        send_default_pii=False,
 
-    # If you wish to associate users to errors (assuming you are using
-    # django.contrib.auth) you may enable sending PII data.
-    send_default_pii=False,
-
-    # By default the SDK will try to use the SENTRY_RELEASE
-    # environment variable, or infer a git commit
-    # SHA as release, however you may want to set
-    # something more human-readable.
-    release="media-editor@1.0.0",
-)
+        # By default the SDK will try to use the SENTRY_RELEASE
+        # environment variable, or infer a git commit
+        # SHA as release, however you may want to set
+        # something more human-readable.
+        release="media-editor@1.0.0",
+    )
 
